474843755188e044d9cd39f97383c17a7934545d
Magdalena Tsolaki <magdalena.tsolaki@gmail.com>
2021-11-04 20:42:36 +0100

    init laravel + vue

---
+++ resources/views/layouts/vue.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>CryptoCademy</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+
+<body class="antialiased">
+
+<div id="app"></div>
+
+<script src="{{ asset('js/app.js') }}"></script>
+</body>
+
+</html>
