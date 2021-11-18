743de05ddd1874ed7a14f18187bb8293b6fbf1c8
Lennart Büttner <lnnrtbttnr@outlook.com>
2021-11-18 17:14:35 +0100

    Add ui auth scaffolding

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/{any}', function () {
     return view('layouts.vue');
 })->where('any', '.*');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
