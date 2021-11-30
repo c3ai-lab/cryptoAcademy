59b2ca41c156e257d174d73b5fae2dab3561c237
André Lorenzen <andrelorenzen@gmx.de>
2021-11-30 23:05:28 +0100

    Fetching news from the backend

--- resources/js/components/NewsCard.vue
+++ resources/js/components/NewsCard.vue
@@ -1,12 +1,10 @@
 <template>
-  <b-card no-body class="main">
+  <b-card no-body class="main" @click="open()">
     <b-card-body>
       <b-card-title>
         <h4 class="card-title">{{ title }}</h4>
       </b-card-title>
-      <b-card-text>
-        {{ text }}
-      </b-card-text>
+      <b-card-text> {{ source }} &ndash; {{ datetime }} </b-card-text>
     </b-card-body>
   </b-card>
 </template>
@@ -22,8 +20,28 @@ export default {
   name: "NewsCard",
 
   props: {
-    title: String,
-    text: String,
+    article: Object,
+  },
+
+  computed: {
+    title: function () {
+      return this.article.newsitem.title;
+    },
+    source: function () {
+      return this.article.newsitem.source.title;
+    },
+    url: function () {
+      return this.article.newsitem.url;
+    },
+    datetime: function () {
+      return new Date(this.article.updated_at).toLocaleString("de-DE");
+    },
+  },
+
+  methods: {
+    open: function () {
+      window.open(this.url, "_blank").focus();
+    },
   },
 };
 </script>
--- resources/js/store/main.js
+++ resources/js/store/main.js
@@ -2,11 +2,13 @@ import Vue from "vue";
 import Vuex from "vuex";
 
 import user from "./modules/user";
+import news from "./modules/news";
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
   modules: {
     user,
+    news,
   },
 });
