c24f163c57e8800387f82c03655b193c7d035c40
Lennart Büttner <lnnrtbttnr@outlook.com>
2021-12-01 19:20:41 +0100

    Styling main plot

--- resources/js/views/TradingView.vue
+++ resources/js/views/TradingView.vue
@@ -1,29 +1,39 @@
 <template>
-    <div v-if="hasLoadedSeries">
-        <h1>
-            <b-form-select
-                v-model="symbol"
-                :options="symbols"
-                style="background: none; outline: none; border: none;"
-                @change="onSymbolChange"
-            ></b-form-select>
-        </h1>
-        <div v-if="hasLoadedPrice" style="margin-left: 6px; font-size: 16px">
-        {{price | usd}}
-        </div>
+    <div>
+        <div v-if="hasLoadedSeries && hasLoadedPrice">
+            <h1>
+                <b-form-select
+                    v-model="symbol"
+                    :options="symbols"
+                    style="background: none; outline: none; border: none;"
+                    @change="onSymbolChange"
+                ></b-form-select>
+            </h1>
+            <div v-if="hasLoadedPrice" style="margin-left: 6px; font-size: 16px">
+            <nice-price
+              style="font-size: 20px"
+              :value="price"
+              currency="usd"
+            />
+            </div>
 
-        <apexchart
-            class="apex-charts"
-            type="line"
-            dir=""
-            :series="series"
-            :options="chartOptions"
-            ref="chart"
-        ></apexchart>
-        <div style="display: flex; justify-content: space-evenly;">
-            <button v-for="(dimension, i) in dimensions" :key="i" class="btn">
-                {{dimension}}
-            </button>
+            <apexchart
+                class="apex-charts"
+                type="line"
+                dir=""
+                :series="series"
+                :options="chartOptions"
+                ref="chart"
+            ></apexchart>
+            <div style="display: flex; justify-content: space-evenly;">
+                <button v-for="(dimension, i) in dimensions" :key="i" class="btn" @click="setDimension(dimension)">
+                    {{dimension}}
+                </button>
+            </div>
+            {{activeDimension}}
+        </div>
+        <div v-else>
+          LOADING
         </div>
     </div>
 </template>
@@ -31,6 +41,7 @@
 <script>
 import { WebsocketClient } from 'binance';
 import { MainClient } from 'binance';
+import NicePrice from '../components/NicePrice.vue';
 
 const binanceRest = new MainClient({
   // Optional (default: false) - when true, response strings are parsed to floats (only for known keys).
@@ -44,18 +55,20 @@ const binanceWs = new WebsocketClient({
 export default {
     data() {
         return {
+          lineColor: "#f1b44c",
             hasLoadedSeries: false,
             hasLoadedPrice: false,
             price: null,
+            activeDimension: '1D',
             dimensions: [
-                '1H',
                 '1D',
                 '1W',
                 '1M',
+                '1Y',
                 'All',
             ],
             wsKey: null,
-            symbol: 'BTCUSDT',
+            symbol: 'SOLUSDT',
             symbols: [
                 'BTCUSDT',
                 'ETHUSDT',
@@ -81,69 +94,115 @@ export default {
                 format: 'hh:mm, dd MMM yy'
                 },
             ][1],
-            series: [{
+            series: [
+              {
                 data: null,
-            }],
+              },
+              {
+                data: null,
+              },
+              {
+                data: null,
+              },
+            ],
         }
     },
 
     computed: {
         chartOptions() {
+            // Filter high and low
+            let high = this.series[0].data.reduce(
+              (prev, current) => (prev[1] > current[1]) ? prev : current
+            )
+            let low = this.series[0].data.reduce(
+              (prev, current) => (prev[1] < current[1]) ? prev : current
+            )
+
+
+
+            // clamp high and low timestamp, that they dont overflow
+            let data = this.series[0].data // for readability
+            let firstMts = data[0][0] // first timestamp
+            let lastMts = data[data.length - 1][0] // last timestamp
+
+            // relative factor: adding 0.015 per char if more chars than 7
+            let lenOfPrice = Math.max(
+              this.$options.filters.usd(data[0][1]).length,
+              this.$options.filters.usd(data[data.length - 1][1]).length,
+            )
+            let offset = 0.03 + (lenOfPrice > 7 ? (lenOfPrice - 7) * 0.015 : 0)
+            let offsetInMts = Math.ceil(data.length * offset) * this.timeframe.duration_mts
+
+            let minMts = firstMts + offsetInMts
+            let maxMts = lastMts - offsetInMts
+
+            let lowIsInFirstHalf = Math.abs(low[0] - data[0][0]) <= Math.abs(low[0] - data[data.length - 1][0])
+            let highIsInFirstHalf = Math.abs(high[0] - data[0][0]) <= Math.abs(high[0] - data[data.length - 1][0])
+
             return {
-               chart: {
-                    width: '100%',
-                    type: 'line',
-                    toolbar: {
-                        show: false,
-                    }
-                },
-                dataLabels: {
-                    enabled: false
+                chart: {
+                    type: "line",
+                    height: '100%',
+                    sparkline: {
+                        enabled: true
+                    },
                 },
                 stroke: {
-                    curve: 'smooth',
-                    width: 2,
-                    dashArray: [0, 0, 3]
+                    curve: "smooth",
+                    width: 2
                 },
-                fill: {
-                    type: 'solid',
-                    opacity: [1],
-                },
-                xaxis: {
-                    type: 'datetime',
-                    labels: {
-                        show: false
-                    },
-                    axisBorder: {
-                        show: false
-                    }
-                },
-                yaxis: {
-                    labels: {
-                        show: false
-                    }
-                },
-                colors: ['#3452e1'],
+                colors: [this.lineColor,"#f1b44c00","#f1b44c00"],
+
                 tooltip: {
-                    x: {
-                        show: true,
-                        format: this.timeframe.format,
-                        formatter: undefined,
+                    custom: ({series, seriesIndex, dataPointIndex, w}) =>  {
+                      return `
+                      <div class="arrow_box"><span>
+                      ${
+                        this.$options.filters.usd(
+                          series[seriesIndex][dataPointIndex]
+                        )
+                      }
+                      </span></div>
+                      `
                     },
-                    y: {
-                        formatter: undefined,
-                        title: {
-                            formatter: (seriesName) => seriesName,
+                    enabledOnSeries: [0],
+                },
+                annotations: {
+                    points: [
+                        {
+                            x: lowIsInFirstHalf ? Math.max(low[0], minMts) : Math.min(low[0], maxMts),
+                            y: low[1],
+                            marker: {
+                              size: 0,
+                            },
+                            label: {
+                              borderColor: "#00000000",
+                              offsetY: 26,
+                              style: {
+                                color: "#00000000",
+                                background: "#00000000"
+                              },
+                              text: this.$options.filters.usd(low[1])
+                            }
                         },
-                    },
-                },
-                animations: {
-                    enabled: true,
-                    easing: 'linear',
-                    dynamicAnimation: {
-                    speed: 1000
-                    }
-                },
+                        {
+                            x: highIsInFirstHalf ? Math.max(high[0], minMts) : Math.min(high[0], maxMts),
+                            y: high[1],
+                            marker: {
+                              size: 0,
+                            },
+                            label: {
+                              borderColor: "#00000000",
+                              offsetY: 4,
+                              style: {
+                                color: "#00000000",
+                                background: "#00000000"
+                              },
+                              text: this.$options.filters.usd(high[1])
+                            }
+                        },
+                    ]
+                }
             }
         }
     },
@@ -152,6 +211,9 @@ export default {
         this.fetchCandles()
     },
     methods: {
+        setDimension(dimension) {
+          this.activeDimension = dimension
+        },
         onSymbolChange() {
             this.hasLoadedPrice = false
 
@@ -170,7 +232,6 @@ export default {
                 if (!Array.isArray(data)) {
                     if (data.eventType === 'kline') {
                         this.wsKey = data.wsKey
-                        console.log(this.wsKey);
                         this.price = data.kline.close
                         this.hasLoadedPrice = true
                         if(this.hasLoadedSeries) {
@@ -195,7 +256,12 @@ export default {
                 interval: this.timeframe.symbol,
                 limit: 50,
             }).then(response => {
-                this.series[0].data = response.map(x => [x[0], x[3]])
+              this.series[0].data = response.map(x => [x[0], Number(x[3])])
+
+              // Ghost series hack for inner padding in chart for labels
+              this.series[1].data = response.map(x => [x[0], x[3]*1.003])
+              this.series[2].data = response.map(x => [x[0], x[3]*0.995])
+
                 this.hasLoadedSeries = true
             }).catch(error => {
                 alert(error)
