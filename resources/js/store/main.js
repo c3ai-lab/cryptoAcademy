2b4e2360fec69f90c425dc66f5c261ca0f7bcbd5
Lennart Büttner <lnnrtbttnr@outlook.com>
2021-12-01 22:52:42 +0100

    Drafting store for exchange data

--- resources/js/store/main.js
+++ resources/js/store/main.js
@@ -2,11 +2,13 @@ import Vue from "vue";
 import Vuex from "vuex";
 
 import user from "./modules/user";
+import coins from "./modules/coins";
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
   modules: {
     user,
+    coins,
   },
 });
---
+++ resources/js/store/modules/coins.js
@@ -0,0 +1,77 @@
+import { DIMENSION_MAP } from "../../constants";
+import { MainClient, WebsocketClient } from 'binance';
+
+const binanceRest = new MainClient({
+  beautifyResponses: true,
+});
+
+const binanceWs = new WebsocketClient({
+  beautify: true,
+});
+
+const state = {
+  eurUsdt: 1,
+};
+
+const getters = {
+  current: (state) => (symbol, dimension) => {
+    if (state[symbol] === undefined || state[symbol][dimension] === undefined) return [];
+
+    return state[symbol][dimension].map(x => [x[0], x[1] / state.eurUsdt]);
+  },
+};
+
+const actions = {
+  init({ commit }) {
+    binanceWs.subscribeKlines('EURUSDT', '1m', 'spot');
+
+    binanceWs.on('formattedMessage', (data) => {
+      if (!Array.isArray(data)) {
+        if (data.eventType === 'kline') {
+          commit('setEurUsdt', data.kline.close);
+        }
+      }
+    })
+  },
+  subscribe({ commit, state }, { symbol, dimension }) {
+    if (state[symbol] === undefined || state[symbol][dimension] === undefined) {
+      binanceRest.getKlines({
+        ...DIMENSION_MAP[dimension],
+        symbol: symbol,
+      }).then(response =>
+        commit('set', { symbol, dimension, data: response.map(x => [x[0], Number(x[3])]) })
+      ).catch(error =>
+        alert(error)
+      );
+
+      // binance.onNewCandle(() => {
+      //   commit('add', symbol, dimension, data);
+      // })
+    }
+  },
+  unsubscribe({ commit }) { },
+};
+
+const mutations = {
+  setEurUsdt(state, eurUsdt) {
+    state.eurUsdt = eurUsdt;
+  },
+  add(state, { symbol, dimension, data }) {
+    state.user = user;
+  },
+  set(state, { symbol, dimension, data }) {
+    console.log(symbol, dimension, data);
+    if (state[symbol] === undefined) state[symbol] = {};
+
+    state[symbol][dimension] = data;
+  },
+};
+
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations,
+};
