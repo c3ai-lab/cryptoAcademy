4c4256c045805ad1be204cdc26dad0b7aa55ad7e
Lennart Büttner <lnnrtbttnr@outlook.com>
2021-12-01 19:16:37 +0100

    Adding component for global price display

---
+++ resources/js/components/NicePrice.vue
@@ -0,0 +1,50 @@
+<template>
+  <div style="display: flex">
+    <div v-if="isUsd" class="pt-1">{{symbol}}</div>
+
+    <div style="font-size: 1.5em">{{digitsBeforeComma}}</div>
+    <div class="pt-1"><span v-if="digitsAfterComma">{{comma}}</span>{{digitsAfterComma}}</div>
+
+    <div v-if="!isUsd" class="pt-1">{{symbol}}</div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: Number,
+    currency: String, // 'usd' or 'eur'
+  },
+  computed: {
+    isUsd() {
+      return this.currency == 'usd'
+    },
+    displayPrice() {
+      return this.$options.filters[this.currency](this.value)
+    },
+    symbol() {
+      return this.isUsd ? '$' : '€'
+    },
+    comma() {
+      return this.isUsd ? '.' : ','
+    },
+    digitsBeforeComma() {
+      let res = this.displayPrice.split(this.comma)[0]
+
+      if (this.isUsd) {
+        res = res.split(this.symbol)[1]
+      }
+
+      return res
+    },
+    digitsAfterComma() {
+      return this.displayPrice.split(this.comma)[1]
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
