48121708e46a8caebed5d25826dcf4fc0f0c6eed
Lennart Büttner <lnnrtbttnr@outlook.com>
2021-12-01 19:20:18 +0100

    Letting components decide where they have margin

--- resources/js/layouts/MainLayout.vue
+++ resources/js/layouts/MainLayout.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="container bg-white px-lg-5 my-lg-5">
-        <div class="row">
-            <div class="col-12">
-                <router-view class="my-5"></router-view>
+    <div class="bg-white">
+        <div>
+            <div>
+                <router-view></router-view>
             </div>
         </div>
     </div>
