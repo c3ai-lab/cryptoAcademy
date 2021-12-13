9bde77c806a1ecb49345e7f3b8ebe4f87d9879a9
Magdalena Tsolaki <magdalena.tsolaki@netzkontor-nord.de>
2021-12-13 17:21:43 +0100

    mailserver

--- docker/prod/init-letsencrypt.sh
+++ docker/prod/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(cryptocademy.app www.cryptocademy.app pma.cryptocademy.app mailhog.cryptocademy.app)
+domains=(cryptocademy.app www.cryptocademy.app pma.cryptocademy.app mailhog.cryptocademy.app mail.cryptocademy.app)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="magdalena.tsolaki@gmail.com" # Adding a valid address is strongly recommended
