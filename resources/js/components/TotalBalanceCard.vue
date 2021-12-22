929ed7ccb05b7e8ec8f6d61862028068e5a99d7b
André Lorenzen <andrelorenzen@gmx.de>
2021-12-22 21:58:30 +0100

    Polished the wallet view
    #74

---
+++ resources/js/components/TotalBalanceCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <b-card>
+    <b-card-text class="balance">
+      {{ totalBalance | eur }}
+    </b-card-text>
+  </b-card>
+</template>
+
+<script>
+export default {
+  name: "TotalBalanceCard",
+
+  computed: {
+    totalBalance() {
+      return this.$store.getters["wallets/totalBalance"]();
+    },
+  },
+
+  created() {
+    this.$store.dispatch("wallets/fetchAll");
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.balance {
+  text-align: center;
+  font-size: 2rem;
+}
+</style>
--- resources/js/filters.js
+++ resources/js/filters.js
@@ -1,57 +1,59 @@
-import Vue from 'vue';
+import Vue from "vue";
 
 function getFractionDigits(value) {
-    if (value < 0.0001) return 8
+  if (value == 0) return 2;
 
-    if (value < 0.001) return 7
+  if (value < 0.0001) return 8;
 
-    if (value < 0.01) return 6
+  if (value < 0.001) return 7;
 
-    if (value < 0.1) return 5
+  if (value < 0.01) return 6;
 
-    if (value < 1) return 4
+  if (value < 0.1) return 5;
 
-    return 2
+  if (value < 1) return 4;
+
+  return 2;
 }
 
-Vue.filter('usd', function (value) {
-    if (typeof value !== "number") {
-        return value;
-    }
-
-    var formatter = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD',
-        minimumFractionDigits: getFractionDigits(value),
-    });
-    return formatter.format(value);
-});
-
-Vue.filter('eur', function (value) {
-    if (typeof value !== "number") {
-        return value;
-    }
-
-    var formatter = new Intl.NumberFormat('de-DE', {
-        style: 'currency',
-        currency: 'EUR',
-        minimumFractionDigits: getFractionDigits(value)
-    });
-    return formatter.format(value);
-});
-
-Vue.filter('symbol', function (value) {
-  return value.split('USDT')[0];
-});
-
-Vue.filter('crypto', function (value) {
+Vue.filter("usd", function (value) {
   if (typeof value !== "number") {
     return value;
-}
+  }
 
-var formatter = new Intl.NumberFormat('de-DE', {
+  var formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: getFractionDigits(value),
+  });
+  return formatter.format(value);
+});
+
+Vue.filter("eur", function (value) {
+  if (typeof value !== "number") {
+    return value;
+  }
+
+  var formatter = new Intl.NumberFormat("de-DE", {
+    style: "currency",
+    currency: "EUR",
+    minimumFractionDigits: getFractionDigits(value),
+  });
+  return formatter.format(value);
+});
+
+Vue.filter("symbol", function (value) {
+  return value.split("USDT")[0];
+});
+
+Vue.filter("crypto", function (value) {
+  if (typeof value !== "number") {
+    return value;
+  }
+
+  var formatter = new Intl.NumberFormat("de-DE", {
     minimumFractionDigits: 8,
     maximumFractionDigits: 8,
-});
-return formatter.format(value);
+  });
+  return formatter.format(value);
 });
